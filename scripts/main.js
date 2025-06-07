5cba62306fc3a9f51e3f34ce6aca0d8ad95f63f4
DeepPurpl3 <caetanomoamed@hotmail.com>
2025-06-07 14:53:20 +0200

    1

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+// ♒♒♒♒♒♒♒♒♒
+// 🌱 Sélection des éléments
+// ♒♒♒♒♒♒♒♒♒
+const taskInput            = document.getElementById("nameTask");
+const dateInput            = document.getElementById("dateTask");
+const descriptionInput     = document.getElementById("zone");
+const addBtn               = document.getElementById("ajouter");
+const triBtn               = document.getElementById("tri");
+const displayDiv           = document.getElementById("displayTask");
+
+
+
+// ♒♒♒♒♒♒♒♒♒
+// 🧠 Variables globales
+// ♒♒♒♒♒♒♒♒♒
+let taskInputValue        = taskInput.value; 
+let dateInputValue        = dateInput.value;
+let descriptionInputValue = descriptionInput.value;
+
+
+
+
+
+// ♒♒♒♒♒♒♒♒♒
+// 🚀 Fonctionnalités
+// ♒♒♒♒♒♒♒♒♒
+
+function displayCard() {
+
+
+}
+
+addBtn.addEventListener('click', function() {
+
+});
+
+
+
+triBtn.addEventListener('click', function() {
+  
+});
